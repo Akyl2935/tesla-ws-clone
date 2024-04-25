0419a520e0f191b41e4b657ea06797d834d98b61
akyl2935 <jpak2935@gmail.com>
2024-04-25 23:58:52 +0600

    feat: test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="bg-[url('https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Model-Y-Desktop-Global.png')] lg:bg-[url('https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Model-Y-Desktop-Global.png')] h-screen bg-cover bg-center">
   <navbar />
     </div>
+    <div></div>
   );
 }
 
