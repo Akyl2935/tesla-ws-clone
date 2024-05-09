c49e40ff7bbc5b18464c1b96aaa416838e983ce9
akyl2935 <jpak2935@gmail.com>
2024-05-09 18:26:57 +0600

    feat: integrated svg files to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,42 @@
 import React from "react";
+import questionSVG from "../assets/questionSVG";
+import userMarkSVG from "../assets/userMarkSVG";
+import internetMarkSVG from "../assets/internetMarkSVG";
 
 const navbar = () => {
   return (
     <div>
+
       <div>
+
         <img />
+
       </div>
 
       <div>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+
+          <li>Vehicles</li>
+          <li>Energy</li>
+          <li>Charging</li>
+          <li>Discover</li>
+          <li>Shop</li>
+
         </ul>
       </div>
+
       <div>
+
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
+
+          <li><questionSVG/></li>
+          <li><userMarkSVG/></li>
+          <li><internetMarkSVG/></li>
+
         </ul>
+
       </div>
+
     </div>
   );
 };
