46dda2d8bc967b85a8469c02d62235a8cc8dfc3d
akyl2935 <jpak2935@gmail.com>
2024-05-12 20:34:18 +0600

    feat: model component

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from "react";
+import { FiChevronDown } from "react-icons/fi";
 
 const Model = () => {
   return (
     <div>
-      <div>
-        <h1>Model Y</h1>
-        <p>From $399/mo at 0.99 APR</p>
+      <div className="absolute inset-x-0 top-[15%] text-center">
+        <h1 className="text-4xl font-bold">Model Y</h1>
+        <p className = "p-2">From <span className="underline underline-offset-4">$399/mo at 0.99 APR</span></p>
       </div>
+
       <div>
         <button>Order Now</button>
         <button>Demo Drive</button>
-    </div>
-    <div>
+      </div>
 
+      <div>
+        <FiChevronDown />
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
 export default Model;
