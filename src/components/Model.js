aec05b93cb5c98fdab9a993d4b49d8b0d7ca7ce9
akyl2935 <jpak2935@gmail.com>
2024-05-12 20:22:32 +0600

    feat: npm react icons installed

--- src/components/Model.js
+++ src/components/Model.js
@@ -7,8 +7,13 @@ const Model = () => {
         <h1>Model Y</h1>
         <p>From $399/mo at 0.99 APR</p>
       </div>
+      <div>
         <button>Order Now</button>
         <button>Demo Drive</button>
+    </div>
+    <div>
+
+    </div>
     </div>
   )
 }
